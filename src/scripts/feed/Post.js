0572c579cab03778893c9ccb9af38d31af124b20
brandonjon26 <brandonjon26@gmail.com>
2021-03-24 15:15:40 -0500

    edit and delte buttons only show for logged in user and author name displays

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,15 +1,24 @@
+import { getLoggedInUser } from "../data/DataManager.js"
 export const Post = (postObject) => {
     return `
       <section class="post">
         <header>
             <h2 class="post__title">${postObject.title}</h2>
         </header>
+        <p> posted by: ${postObject.user.name}</p>
         <img class="post__image" src="${postObject.imageURL}" />
         <p> userId: ${postObject.userId}</p>
         <p> description: ${postObject.description}</p>
-        <p> timestamp: ${postObject.timestamp}</p>
-        <button id="edit__${postObject.id}">Edit</button>
-        <button id="delete__${postObject.id}">Delete</button> 
-      </section>
-    `
-  }
+        ${postObject.user.id === getLoggedInUser().id
+          ?` <button id="edit__${postObject.id}">Edit</button>
+          <button id="delete__${postObject.id}">Delete</button> `
+          :""
+        }
+        </section>
+        `
+      }
+      // <p> timestamp: ${postObject.timestamp}</p>
+
+      // ternary statement on line 12 is a way to write a conditional statement
+      // what comes after the question mark is when the condition is true
+      // the colon represents if the codition is false
